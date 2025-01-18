4ea1b6348232d3b076e26e4553d3902a371fd700
Petr Garkavenko <fat-bee@mail.ru>
2025-01-18 15:27:03 +1000

    Создание моделей и настройка админки

--- test_task/wallet/admin.py
+++ test_task/wallet/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Wallet, Operation
+
+
+# Регистрация модели кошелька в админке
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ('wallet_uuid', 'balance')  # Отображаемые поля
+    search_fields = ('wallet_uuid',)  # Поля для поиска
+
+
+# Регистрация модели операции в админке
+@admin.register(Operation)
+class OperationAdmin(admin.ModelAdmin):
+    list_display = ('wallet', 'operation_type', 'amount', 'created_at')  # Отображаемые поля
+    search_fields = ('wallet__wallet_uuid', 'operation_type', 'amount', 'created_at')  # Поля для поиска
+    list_filter = ('wallet', 'operation_type', 'amount', 'created_at')  # Фильтры в списке объектов
+    ordering = ('-created_at',)  # Сортировка по умолчанию
+
--- test_task/wallet/models.py
+++ test_task/wallet/models.py
@@ -1,3 +1,28 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+# Модель кошелька
+class Wallet(models.Model):
+    # UUID кошелька
+    wallet_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # Баланс кошелька
+    balance = models.DecimalField(max_digits=20, decimal_places=2, default=0.00)
+
+
+# Модель операций(истории операций)
+class Operation(models.Model):
+    # Подкласс для выбора вариантов операции
+    class OperationType(models.TextChoices):
+        # Положить деньги на баланс
+        DEPOSIT = 'DEPOSIT'
+        # Снять деньги с баланса
+        WITHDRAW = 'WITHDRAW'
+    # Ссылка на кошелек
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name='operations')
+    # Тип операции(выбор положить/снять)
+    operation_type = models.CharField(max_length=8, choices=OperationType.choices)
+    # Сумма операции
+    amount = models.DecimalField(max_digits=20, decimal_places=2, default=0.00)
+    # Время операции
+    created_at = models.DateTimeField(auto_now_add=True)
