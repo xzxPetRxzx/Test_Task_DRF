6bef39b8674e009fbb4ff7a4caea545b9a26d9d1
Petr Garkavenko <fat-bee@mail.ru>
2025-01-20 15:32:19 +1000

    Добавил переменные окружения для более гибкой настройки контейнеров и приложения

--- test_task/settings.py
+++ test_task/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_HOST', 'db'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
